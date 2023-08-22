022086cdbc8bf7d7acb692d855d7d69b078c4117
Dmytro Semenko <45269867+DmytroSemenko@users.noreply.github.com>
2023-08-23 01:23:42 +0300

    Add project files.

---
+++ src/Fanitty.Server.API/Extensions/Authentication/FirebaseAuthenticationExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace Fanitty.Server.API.Extensions.Authentication;
+
+public static class FirebaseAuthenticationExtensions
+{
+    public static void AddFirebaseAuthentication(this WebApplicationBuilder builder)
+    {
+        var audience = builder.Configuration.GetValue<string>("FirebaseProjectId");
+        builder.Services.AddAuthentication(o =>
+        {
+            o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            o.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(o =>
+        {
+            o.RefreshOnIssuerKeyNotFound = true;
+            o.MetadataAddress = $"https://securetoken.google.com/{audience}/.well-known/openid-configuration";
+            o.Audience = audience;
+        });
+    }
+}
